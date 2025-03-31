5bda5b06b0783aa2ae879f6489b706e0d701df61
ESDunlap <dunlape3@my.erau.edu>
2025-03-31 11:57:17 -0700

    Create placementLoops.h

---
+++ placementLoops.h
@@ -0,0 +1,13 @@
+#ifndef FULL_PLACEMENT_LOOP_H
+#define FULL_PLACEMENT_LOOP_H
+#ifndef AI_PLACEMENT_LOOP_H
+#define AI_PLACEMENT_LOOP_H
+#define COLUMNS 10
+#define ROWS 10
+
+int fullPlacementLoop(char array[ROWS][COLUMNS]) 
+int aiPlacementLoop(char array[ROWS][COLUMNS]) 
+
+#define MAX_BUFFER_Size 1024
+
+#endif
