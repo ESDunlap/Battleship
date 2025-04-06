96a43bd894edd490f397cc29ce9dbd2413438d41
ESDunlap <dunlape3@my.erau.edu>
2025-04-05 22:47:44 -0700

    ai difficulty added

---
+++ game_manager.c
@@ -0,0 +1,121 @@
+#include "game_manager.h"
+
+//#define COLUMNS 10
+//#define ROWS 10
+
+/*
+	File: game_manager.c
+	Authors: Kaeden Daly, Erik Dunlap
+	Course: CS125
+	Assignement: Final Project
+	Date: 03/31/25
+	References: 
+*/
+
+int main()
+{
+  printf("Welcome to Battleship\n");
+  printf("By Kaeden Daly and Erik Dunlap\n\n");
+  
+  //start:
+  
+  int difficulty;
+  printf("Please choose difficulty\n\n");
+  printf("Type \"1\" for Player vs Player\n");
+  printf("Type \"2\" for Easy AI\n"); //Completely Random
+  printf("Type \"3\" for Medium AI\n"); //Random then adjacent Tiles
+  printf("Type \"4\" for Hard AI\n"); //Random then known Ships Tiles
+  printf("Type \"5\" for Very Hard AI\n"); //Known Ship Tiles
+  printf("Type \"6\" for Impossible AI\n"); //Known Ship Tiles, Goes First
+  printf("...: ");
+  while(scanf("%d", &difficulty) != 1 || difficulty < 1 || difficulty > 6)
+  {
+  	while (getchar()!= '\n');
+    printf("Error: Invalid input\n");
+    printf("Please Try Again: ");
+  }
+  while (getchar()!= '\n');
+  
+  char player1[ROWS][COLUMNS];
+  char player2[ROWS][COLUMNS];
+  char ai[ROWS][COLUMNS];
+  
+  printf("\n\n\n");
+  
+  switch(difficulty)
+  {
+    case 1:
+    {
+    	createArray(player1);
+			createArray(player2);
+			
+      printf("Player 1: \n\n");
+      fullPlacementLoop(player1);
+      
+      printf("Player 2: \n\n");
+      fullPlacementLoop(player2);
+      
+      game(player1, player2);
+      break;
+    }
+    case 2:
+    {
+    }
+    case 3:
+    {
+    }
+    case 4:
+    {
+    }
+    case 5:
+    {
+    	createArray(player1);
+			createArray(ai);
+			
+      printf("Player 1: ");
+      fullPlacementLoop(player1);
+      aiPlacementLoop(ai);
+      
+      aiGame(player1, ai, difficulty);
+      break;
+    }
+    case 6:
+    {
+    	createArray(player1);
+			createArray(ai);
+			
+      printf("Player 1: \n\n");
+      fullPlacementLoop(player1);
+      aiPlacementLoop(ai);
+      
+      aiLevel4Turn(player1);
+      aiGame(player1, ai, difficulty);
+      break;
+    }
+  }
+  
+  
+  printf("Play Again (Y/N)");
+  char userInput;
+  while(scanf("%c", &userInput) == 0)
+  {
+    printf("Invalid Character\n");
+    while(getchar()!= '\n');
+  }
+  userInput = tolower(userInput);
+  
+  if(userInput == 'y')
+  {
+    //goto start;
+  }
+  else if(userInput == 'n')
+  {
+    printf("\n\nApplication Shutting Down\n\n");
+  }
+  else
+  {
+    printf("Error: Invalid Input");
+  }
+
+  
+}
---
+++ game_manager.h
@@ -0,0 +1,18 @@
+//File: game_Manager.h
+
+#ifndef GAME_MANAGER_H
+#define GAME_MANAGER_H
+
+#include <stdio.h>
+#include "battleShipPlacement.h"
+#include "fire_input.h"
+#include "aiLevels.h"
+
+#define COLUMNS 10
+#define ROWS 10
+
+int main();
+
+#define MAX_BUFFER_SIZE 1024
+
+#endif
